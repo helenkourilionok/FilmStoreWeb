428bb6d1399f868a6297d830c9c811c6a1e56337
Helen_Kourilionok <julia-lena-dru@yandex.ru>
2016-08-06 12:24:13 +0400

    filmStoreWeb commit

--- src/by/training/filmstore/dao/FilmDirectorDAO.java
+++
@@ -1,8 +0,0 @@
-package by.training.filmstore.dao;
-
-import by.training.filmstore.dao.exception.FilmStoreDAOException;
-import by.training.filmstore.entity.FilmDirector;
-
-public interface FilmDirectorDAO extends AbstractDAO<FilmDirector, Short>{
-	FilmDirector findByFIO(String fio) throws FilmStoreDAOException;
-}
